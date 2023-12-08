cd6bf71457b3751825e41b459a8fa76ae2cdfdab
rahulshenoys1 <rahul.shenoy@exathought.com>
2023-12-08 11:48:29 +0530

    Second changes in the opertor file

---
+++ operators.js
@@ -0,0 +1,84 @@
+// try out all the arithmetic operators and print out the results
+/*
+let x=10;
+let y=4;
+
+let result1 = x+y;
+let result2 = x-y;
+let result3 = x*y;
+let result4 = x/y;
+let result5 = x%y;
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
+
+let result6 = ++x;
+console.log(result6);
+
+let result7 = x++;
+console.log(result7);
+
+console.log(++x);
+console.log(x++);
+
+//2. operators in the printing statements
+
+console.log("1"+2+3);
+console.log(6+2+"4"); // addes then it will be converted to string
+console.log('2'+2+"2");
+console.log('2'+2);
+console.log('a'+'b');
+console.log('a'+"b");
+
+//3. change the syntax a bit 
+let b =10;
+b = b +10;
+console.log(b);
+
+let b =10;
+b+=10;
+console.log(b);
+
+//4th question is no use because variables we can use is the let, var and const in the JS
+
+//5. use of the 2's compliment 
+let i =100;
+console.log(i<<1);// 200
+console.log(i<<2);//400
+console.log(i<<3);//800
+console.log(i>>1);//50
+console.log(i>>2);//25
+console.log(i>>3);//12 
+
+let i =55;
+let j= 456
+
+console.log(j/i);//8.2909
+console.log(j%i);//16
+
+// 8. check the testcase below 
+console.log(2>>1);//1
+console.log(6>>2);//1
+console.log(-2>>1);//-1
+
+//9. check the testcse below  
+console.log(10+1);
+console.log(012*2); //this leads to an error octa can't be used
+console.log(0xAB /3);
+
+//10. Check the 2's compiliment 
+let x =10;
+console.log(~x);//-11
+
+let a=100;
+console.log(~a);-101*/
+
+//11. Execute the code below
+let x =10;
+let y = 6;
+console.log(x|y);
+
+let 
